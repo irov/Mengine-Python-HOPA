74a58f689beeac76f2fb5cd4e57a0fc9af99207c
Yeva <55158011+eclipse7723@users.noreply.github.com>
2023-03-20 23:17:02 +0100

    check if params correct

--- Scripts/HOPA/Macro/MacroSpellAmuletWaitState.py
+++ Scripts/HOPA/Macro/MacroSpellAmuletWaitState.py
@@ -2,25 +2,35 @@ from Foundation.SceneManager import SceneManager
 from HOPA.Macro.MacroCommand import MacroCommand
 from HOPA.SpellsManager import SpellsManager
 from HOPA.ZoomManager import ZoomManager
+from HOPA.Entities.SpellAmulet.SpellAmulet import AmuletPowerButton, Amulet
 
 
 class MacroSpellAmuletWaitState(MacroCommand):
     """ Confluence: https://bit.ly/3FuSiHL """
 
     VALID_TARGETS = ['amulet', 'stone']
+    VALID_STATE_ARGS = {
+        'amulet':
+            [Amulet.IDLE, Amulet.HIDE, Amulet.OPEN, AmuletPowerButton.AIM, AmuletPowerButton.FAIL],
+        'stone':
+            [AmuletPowerButton.APPEAR, AmuletPowerButton.IDLE, AmuletPowerButton.SELECT, AmuletPowerButton.READY]
+    }
 
     def _onValues(self, values):  # SpellAmuletWaitState <target> <state> [power_type]
-        # MACRO Amulet aim
         self.target = values[0]
         self.state = values[1]
         self.power_type = values[2] if len(values) > 2 else None
 
-    def _onInitialize(self, *args, **kwds):
+    def _onInitialize(self, *args, **kwargs):
         if _DEVELOPMENT is True:
             if self.target not in self.VALID_TARGETS:
-                self.initializeFailed("MacroSpellAmuletWaitState: invalid target {!r}, use one of them: {}".format(self.target, self.VALID_TARGETS))
+                self.initializeFailed("invalid target {!r}, use one of them: {}".format(self.target, self.VALID_TARGETS))
             if self.power_type is not None and SpellsManager.getSpellAmuletStoneParam(self.power_type) is None:
-                self.initializeFailed("MacroSpellAmuletWaitState: not found power_type '{}'".format(self.power_type))
+                self.initializeFailed("not found power_type '{}'".format(self.power_type))
+            if self.target == "amulet" and self.power_type is not None:
+                self.initializeFailed("target 'amulet' hasn't power_type. Remove '{}' from macro".format(self.power_type))
+            if self.state not in self.VALID_STATE_ARGS[self.target]:
+                self.initializeFailed("target '{}' - wrong state '{}', available: {}".format(self.target, self.state, self.VALID_STATE_ARGS[self.target]))
 
     def __checkLocation(self):
         cur_scene = SceneManager.getCurrentSceneName()
@@ -35,7 +45,7 @@ class MacroSpellAmuletWaitState(MacroCommand):
                 return False
         return True
 
-    def __filterAmuletStateUpdate(self, state):  # SpellAmulet
+    def __filterAmuletStateUpdate(self, state):  # SpellAmulet.Amulet
         if self.__checkLocation() is False:
             return False
 
@@ -44,7 +54,7 @@ class MacroSpellAmuletWaitState(MacroCommand):
             return False
         return True
 
-    def __filterSpellUIStateUpdate(self, button, state):  # SpellAmuletPowerButton
+    def __filterSpellUIStateUpdate(self, button, state):  # SpellAmulet.AmuletPowerButton
         if self.__checkLocation() is False:
             return False
 
