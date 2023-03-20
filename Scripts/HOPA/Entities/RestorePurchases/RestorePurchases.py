48f3b43b7b52337d979ff5b6b666487ef8f002f1
Yeva <55158011+eclipse7723@users.noreply.github.com>
2023-03-20 17:33:21 +0100

    wip

--- Scripts/HOPA/Entities/RestorePurchases/RestorePurchases.py
+++ Scripts/HOPA/Entities/RestorePurchases/RestorePurchases.py
@@ -3,8 +3,14 @@ from Foundation.TaskManager import TaskManager
 from Foundation.Systems.SystemAppleServices import SystemAppleServices
 from Foundation.Systems.SystemGoogleServices import SystemGoogleServices
 
+BUTTON_NAME = "Movie2Button_RestorePurchases"
+
 
 class RestorePurchases(BaseEntity):
+
+    def __init__(self):
+        super(RestorePurchases, self).__init__()
+        self.button = None
 
     @staticmethod
     def callRestorePurchases():
@@ -16,10 +22,18 @@ class RestorePurchases(BaseEntity):
             Trace.msg_err("Not found module to call Restore Purchases :'(")
 
     def _onPreparation(self):
+        if self.object.hasObject(BUTTON_NAME) is False:
+            Trace.log("Entity", 0, "Demon {} should has object {}".format(self.object.getName(), BUTTON_NAME))
+            return
+
+        self.button = self.object.getObject(BUTTON_NAME)
+
         with TaskManager.createTaskChain(Name="RestorePurchasesButton", Repeat=True) as tc:
-            tc.addTask("TaskMovie2ButtonClick", Movie2ButtonName="Movie2Button_RestorePurchases")
+            tc.addTask("TaskMovie2ButtonClick", Movie2Button=self.button)
             tc.addFunction(self.callRestorePurchases)
 
     def _onDeactivate(self):
         if TaskManager.existTaskChain("RestorePurchasesButton") is True:
             TaskManager.cancelTaskChain("RestorePurchasesButton")
+
+        self.button = None
