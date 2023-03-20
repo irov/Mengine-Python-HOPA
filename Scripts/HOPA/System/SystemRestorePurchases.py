78f94fc64fdd65fc2d02c02a13cd70877717b95e
Yeva <55158011+eclipse7723@users.noreply.github.com>
2023-03-20 11:36:43 +0100

    add system that opens RestorePurchases

---
+++ Scripts/HOPA/System/SystemRestorePurchases.py
@@ -0,0 +1,36 @@
+from Foundation.System import System
+
+
+class SystemRestorePurchases(System):
+
+    OPEN_TRIGGERS = ["InGameMenu"]
+
+    def _onRun(self):
+        self.addObserver(Notificator.onLayerGroupEnable, self._cbLayerGroupEnable)
+        self.addObserver(Notificator.onLayerGroupDisable, self._cbLayerGroupDisable)
+        return True
+
+    def _cbLayerGroupEnable(self, group_name):
+        if group_name not in self.OPEN_TRIGGERS:
+            return False
+
+        if self.existTaskChain("SystemRestorePurchases_Disappear") is True:
+            self.removeTaskChain("SystemRestorePurchases_Disappear")
+
+        if self.existTaskChain("SystemRestorePurchases_Appear") is True:
+            self.removeTaskChain("SystemRestorePurchases_Appear")
+        with self.createTaskChain(Name="SystemRestorePurchases_Appear") as tc:
+            tc.addTask("TaskSceneLayerGroupEnable", LayerName="RestorePurchases", Value=True)
+
+        return False
+
+    def _cbLayerGroupDisable(self, group_name):
+        if group_name not in self.OPEN_TRIGGERS:
+            return False
+
+        if self.existTaskChain("SystemRestorePurchases_Disappear") is True:
+            self.removeTaskChain("SystemRestorePurchases_Disappear")
+        with self.createTaskChain(Name="SystemRestorePurchases_Disappear", Repeat=False) as tc:
+            tc.addTask("TaskSceneLayerGroupEnable", LayerName="RestorePurchases", Value=False)
+
+        return False
