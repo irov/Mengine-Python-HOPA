04385a04161c92b82bf08e9e91f6b9bac0001e41
Yeva <55158011+eclipse7723@users.noreply.github.com>
2023-05-04 16:41:44 +0200

    add release purchased

--- Scripts/HOPA/Entities/SpecialPromotion/SpecialPromotion.py
+++ Scripts/HOPA/Entities/SpecialPromotion/SpecialPromotion.py
@@ -43,7 +43,7 @@ class SpecialPromotion(BaseEntity):
         for alias in aliases:
             Mengine.setTextAlias("", alias, "ID_EMPTY")
 
-        self.__loadMovies()
+        self._loadMovies()
 
     def _onActivate(self):
         self._observers.append(Notification.addObserver(Notificator.onProductsUpdateDone, self._onProductsUpdateDone))
@@ -67,28 +67,37 @@ class SpecialPromotion(BaseEntity):
 
     # /////////////
 
-    def __loadMovies(self):
-        def _getMovies(dict_in, dict_out):
-            for key, movie_name in dict_in.items():
-                if self.object.hasObject(movie_name):
-                    movie = self.object.getObject(movie_name)
-                    movie.setInteractive(True)
-                    dict_out[key] = movie
-                else:
-                    _Log("{}:{} not inited ({} hasn't this object)".format(key, movie_name, self.object.getName()), err=True)
+    def _getMovies(self, dict_in, dict_out, optional=False):
+        for key, movie_name in dict_in.items():
+            if self.object.hasObject(movie_name):
+                movie = self.object.getObject(movie_name)
+                movie.setInteractive(True)
+                dict_out[key] = movie
+                continue
 
+            if optional is True:
+                continue
+
+            _Log("{}:{} not inited ({} hasn't this object)".format(key, movie_name, self.object.getName()), err=True)
+
+    def _loadMovies(self):
         content = {
             "window": "Movie2_Window",
             "purchase": "Movie2Button_Purchase",
             "close": "Movie2Button_Close",
         }
-        _getMovies(content, self.content)
+        self._getMovies(content, self.content)
+
+        optional_content = {
+            "restore": "Movie2Button_Restore",
+        }
+        self._getMovies(optional_content, self.content, optional=True)
 
         effects = {
             "appear": "Movie2_Appear",
             "disappear": "Movie2_Disappear",
         }
-        _getMovies(effects, self.effects)
+        self._getMovies(effects, self.effects)
 
         icon_slot = None
         if self.content["window"].hasSlot("icon") is False:
@@ -110,6 +119,8 @@ class SpecialPromotion(BaseEntity):
 
         self._attach("window", "close", "close")
         self._attach("window", "purchase", "purchase")
+        if "restore" in self.content:
+            self._attach("window", "purchase", "restore")
 
         self.attachToEffect("appear")
         self.setEnableWindow(False)
@@ -143,7 +154,7 @@ class SpecialPromotion(BaseEntity):
             self.ALIAS_REWARD_ENERGY:
                 [params.id_reward_energy, MonetizationManager.getProductReward(params.id, "Energy")]
         }
-        self.__setTexts(texts)
+        self._setTexts(texts)
 
     def _setRewardsPlate(self, tag):
         if self.object.hasPrototype("Movie2_Rewards") is False:
@@ -186,27 +197,28 @@ class SpecialPromotion(BaseEntity):
 
     def _attach(self, parent_name, slot_name, child_name):
         if self.content[parent_name].hasMovieSlot(slot_name) is False:
-            Trace.log("Entity", 0, "__loadMovies - not found slot '{}' on '{}'".format(slot_name, parent_name))
+            Trace.log("Entity", 0, "_loadMovies - not found slot '{}' on '{}'".format(slot_name, parent_name))
             return False
         slot = self.content[parent_name].getMovieSlot(slot_name)
 
         if child_name not in self.content:
-            Trace.log("Entity", 0, "__loadMovies - key '{}' not found".format(child_name))
+            Trace.log("Entity", 0, "_loadMovies - key '{}' not found".format(child_name))
             return False
-        child = self.content.get(child_name)
+        child = self.content[child_name]
 
         child_node = child.getEntityNode()
         child_node.removeFromParent()
         slot.addChild(child_node)
         return True
 
-    def __setTexts(self, texts_dict):
+    def _setTexts(self, texts_dict):
         """ :param texts_dict: { alias_id: [text_id, *args] }
                 dict where key is alias and value is a list where first val is text_id and other is args """
         for alias, args in texts_dict.items():
-            self.__setText("", alias, *args)
+            self._setText("", alias, *args)
 
-    def __setText(self, env, alias, text_id, *args):
+    @staticmethod
+    def _setText(env, alias, text_id, *args):
         if text_id is None:
             return
 
@@ -293,15 +305,12 @@ class SpecialPromotion(BaseEntity):
             Trace.log("Entity", 0, "SpecialPromotion start failed - not found special promotion for product with id {}".format(special_prod_id))
             return False
 
-        if self.setup(params.tag) is False:
+        if MonetizationManager.hasProductInfo(params.id) is False:
+            Trace.log("Entity", 0, "SpecialPromotion start failed [{}] - not found product with id {}".format(special_prod_id, params.id))
             return False
 
-        SystemMonetization = SystemManager.getSystem("SystemMonetization")
-        # if we got an alias product id - need real:
-        product_id = MonetizationManager.getProductInfo(special_prod_id).id
-
-        def scopeSuccess(source):
-            source.addFunction(EVENT_GET_PURCHASED)
+        if self.setup(params.tag) is False:
+            return False
 
         with TaskManager.createTaskChain(Name="SpecialPromotion") as tc:
             with tc.addParallelTask(2) as (tc_show, tc_fade):
@@ -309,8 +318,7 @@ class SpecialPromotion(BaseEntity):
                 tc_fade.addTask("AliasFadeIn", FadeGroupName="FadeUI", To=0.5, Time=250.0, ReturnItem=False)
 
             with tc.addRepeatTask() as (purchase, until):
-                purchase.addTask("TaskMovie2ButtonClick", Movie2Button=self.content["purchase"])
-                purchase.addScope(SystemMonetization.scopePay, product_id, scopeSuccess=scopeSuccess)
+                purchase.addScope(self._scopePurchaseClick)
 
                 with until.addRaceTask(2) as (close, interrupt):
                     close.addTask("TaskMovie2ButtonClick", Movie2Button=self.content["close"])
@@ -321,3 +329,36 @@ class SpecialPromotion(BaseEntity):
                 tc_fade.addTask("AliasFadeOut", FadeGroupName="FadeUI", From=0.5, Time=250.0)
 
         return True
+
+    def _scopePurchaseClick(self, source):
+        def scopeSuccess(src):
+            src.addFunction(EVENT_GET_PURCHASED)
+
+        params = self.params[self.current_tag]
+        SystemMonetization = SystemManager.getSystem("SystemMonetization")
+
+        # if we got an alias product id - need real:
+        product_id = MonetizationManager.getProductRealId(params.id)
+
+        if product_id is None:
+            source.addBlock()
+            return
+
+        if SystemMonetization.isPurchaseDelayed(product_id) is True:
+            if "restore" in self.content:
+                self.content["restore"].setEnable(True)
+                self.content["purchase"].setEnable(False)
+
+            source.addTask("TaskMovie2ButtonClick", Movie2Button=self.content.get("restore", "purchase"))
+            with source.addParallelTask(2) as (purchased, release):
+                purchased.addListener(Notificator.onPaySuccess, Filter=lambda prod_id: prod_id == product_id)
+                purchased.addScope(scopeSuccess)
+                release.addNotify(Notificator.onReleasePurchased, product_id)
+        else:
+            if "restore" in self.content:
+                self.content["restore"].setEnable(False)
+                self.content["purchase"].setEnable(True)
+
+            source.addTask("TaskMovie2ButtonClick", Movie2Button=self.content["purchase"])
+            source.addScope(SystemMonetization.scopePay, product_id, scopeSuccess=scopeSuccess)
+
