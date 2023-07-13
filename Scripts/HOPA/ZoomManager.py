4ca7efd27e4a94663520e12188b305031aeef7ff
Ryuzaki <71939354+Ryuzak-i@users.noreply.github.com>
2023-07-13 19:33:35 +0300

    remove print

--- Scripts/HOPA/ZoomManager.py
+++ Scripts/HOPA/ZoomManager.py
@@ -336,9 +336,6 @@ class ZoomManager(object):
 
         Notification.notify(Notificator.onZoomOpen, zoomGroupName)
 
-        if _DEVELOPMENT is True:
-            Trace.msg("<ZoomManager> open zoom '%s'" % zoomGroupName)
-
     @staticmethod
     def closeZoom(zoomGroupName):
         BlockGameScenes = SceneManager.isBlockGameScenes()
