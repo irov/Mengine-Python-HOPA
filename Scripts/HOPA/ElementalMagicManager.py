4be0ff3b5d9de00415a3ba16463632459212625e
Ryuzaki <71939354+Ryuzak-i@users.noreply.github.com>
2023-08-11 19:27:40 +0300

    WIP: add base for Manager and DB

--- Scripts/HOPA/ElementalMagicManager.py
+++ Scripts/HOPA/ElementalMagicManager.py
@@ -1,5 +1,32 @@
 from Foundation.Manager import Manager
+from Foundation.DatabaseManager import DatabaseManager
 
 
 class ElementalMagicManager(Manager):
-    pass
+    class Params(object):
+        if _DEVELOPMENT:
+            def __repr__(self):
+                return "<{}: {}>".format(self.__class__.__name__, self.__dict__)
+
+    class ConfigParams(Params):
+        def __init__(self, records):
+            pass
+
+    class ElementsParams(Params):
+        def __init__(self, records):
+            pass
+
+    class MacroUsageParams(Params):
+        def __init__(self, records):
+            pass
+
+    class MoviesParams(Params):
+        def __init__(self, records):
+            pass
+
+    @staticmethod
+    def loadParams(module, name):
+        records = DatabaseManager.getDatabaseRecords(module, name)
+        # for record in records:
+
+        return True
