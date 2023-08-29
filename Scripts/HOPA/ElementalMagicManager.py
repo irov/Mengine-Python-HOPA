9477c0feea203f7cadaf403f4dbbdda687abd5f6
Ryuzaki <71939354+Ryuzak-i@users.noreply.github.com>
2023-08-29 19:46:22 +0300

    wip ElementalMagicManager params

--- Scripts/HOPA/ElementalMagicManager.py
+++ Scripts/HOPA/ElementalMagicManager.py
@@ -3,6 +3,18 @@ from Foundation.DatabaseManager import DatabaseManager
 
 
 class ElementalMagicManager(Manager):
+    __PARAMS_TABLE_NAMES = {
+        "config": "ElementalMagic_Config",
+        "elements": "ElementalMagic_Elements",
+        "macro_usage": "ElementalMagic_MacroUsage",
+        "movies": "ElementalMagic_Movies",
+    }
+
+    s_configs = {}
+    s_elements = {}
+    s_macro_usages = {}
+    s_movies = {}
+
     class Params(object):
         if _DEVELOPMENT:
             def __repr__(self):
@@ -10,23 +22,77 @@ class ElementalMagicManager(Manager):
 
     class ConfigParams(Params):
         def __init__(self, records):
-            pass
+            self.config = {}
+
+            for record in records:
+                key = record.get("Key")
+                value = record.get("Value")
+
+                if key is not None:
+                    self.config[key] = value
 
     class ElementsParams(Params):
-        def __init__(self, records):
-            pass
+        def __init__(self, record):
+            self.element = ElementalMagicManager.getRecordValue(record, "Element")
 
     class MacroUsageParams(Params):
-        def __init__(self, records):
-            pass
+        def __init__(self, record):
+            self.elemental_magic_id = ElementalMagicManager.getRecordValue(record, "ElementalMagic_ID")
+            self.element = ElementalMagicManager.getRecordValue(record, "Element")
+            self.limited = ElementalMagicManager.getRecordValue(record, "Limited")
 
     class MoviesParams(Params):
-        def __init__(self, records):
-            pass
+        def __init__(self, record):
+            self.element = ElementalMagicManager.getRecordValue(record, "Element")
+            self.movie2_appear = ElementalMagicManager.getRecordValue(record, "Movie2_Appear")
+            self.movie2_idle = ElementalMagicManager.getRecordValue(record, "Movie2_Idle")
+            self.movie2_use = ElementalMagicManager.getRecordValue(record, "Movie2_Use")
+            self.movie2_disappear = ElementalMagicManager.getRecordValue(record, "Movie2_Disappear")
+
+    @staticmethod
+    def loadConfig(records):
+        params = ElementalMagicManager.ConfigParams(records)
+        ElementalMagicManager.s_configs = params.config
+
+    @staticmethod
+    def loadElements(records):
+        for record in records:
+            param = ElementalMagicManager.ElementsParams(record)
+            if param.element is not None:
+                ElementalMagicManager.s_elements[param.element] = param
+
+    @staticmethod
+    def loadMacroUsage(records):
+        for record in records:
+            param = ElementalMagicManager.MacroUsageParams(record)
+            if param.elemental_magic_id is not None:
+                ElementalMagicManager.s_macro_usages[param.elemental_magic_id] = param
+
+    @staticmethod
+    def loadMovies(records):
+        for record in records:
+            param = ElementalMagicManager.MoviesParams(record)
+            if param.element is not None:
+                ElementalMagicManager.s_movies[param.element] = param
 
     @staticmethod
     def loadParams(module, name):
         records = DatabaseManager.getDatabaseRecords(module, name)
-        # for record in records:
+
+        if name == ElementalMagicManager.__PARAMS_TABLE_NAMES["config"]:
+            ElementalMagicManager.loadConfig(records)
+            print(ElementalMagicManager.s_configs)
+
+        if name == ElementalMagicManager.__PARAMS_TABLE_NAMES["elements"]:
+            ElementalMagicManager.loadElements(records)
+            print(ElementalMagicManager.s_elements)
+
+        if name == ElementalMagicManager.__PARAMS_TABLE_NAMES["macro_usage"]:
+            ElementalMagicManager.loadMacroUsage(records)
+            print(ElementalMagicManager.s_macro_usages)
+
+        if name == ElementalMagicManager.__PARAMS_TABLE_NAMES["movies"]:
+            ElementalMagicManager.loadMovies(records)
+            print(ElementalMagicManager.s_movies)
 
         return True
