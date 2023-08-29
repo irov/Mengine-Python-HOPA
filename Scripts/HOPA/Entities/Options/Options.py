26beb6547451b7eecca3da729a7d0b5aa86894e6
Yeva <55158011+eclipse7723@users.noreply.github.com>
2023-08-29 20:04:40 +0300

    add movie2checkboxes integration

--- Scripts/HOPA/Entities/Options/Options.py
+++ Scripts/HOPA/Entities/Options/Options.py
@@ -13,6 +13,7 @@ BUTTON_LANGUAGE_SELECT_NAME = "Movie2Button_LanguageSelect"
 
 class Options(BaseEntity):
     s_start_volume_values = []
+    s_start_check_box_values = {}
     startMusicVolumeValue = 0
     startSoundVolumeValue = 0
     startVoiceVolumeValue = 0
@@ -24,10 +25,10 @@ class Options(BaseEntity):
         Type.addAction(Type, "MusicVolume", Activate=True, Update=Options._updateMusicVolume)
         Type.addAction(Type, "SoundVolume", Activate=True, Update=Options._updateSoundVolume)
         Type.addAction(Type, "VoiceVolume", Activate=True, Update=Options._updateVoiceVolume)
-        Type.addAction(Type, "Mute", Update=Options._updateMute)
-        Type.addAction(Type, "Fullscreen", Update=Options._updateFullscreen)
-        Type.addAction(Type, "Widescreen", Update=Options._updateWidescreen)
-        Type.addAction(Type, "Cursor", Update=Options.__updateCursor)
+        Type.addAction(Type, "Mute", Activate=True, Update=Options._updateMute)
+        Type.addAction(Type, "Fullscreen", Activate=True, Update=Options._updateFullscreen)
+        Type.addAction(Type, "Widescreen", Activate=True, Update=Options._updateWidescreen)
+        Type.addAction(Type, "Cursor", Activate=True, Update=Options.__updateCursor)
 
     def __init__(self):
         super(Options, self).__init__()
@@ -61,6 +62,14 @@ class Options(BaseEntity):
             Options.startSoundVolumeValue,
             Options.startVoiceVolumeValue
         ]
+
+        Options.s_start_check_box_values = {
+            "Arrow": Mengine.getCurrentAccountSettingBool('CustomCursor'),
+            "FullScreen": Mengine.getCurrentAccountSettingBool('Fullscreen'),
+            "WideScreen": Mengine.getCurrentAccountSettingBool('Widescreen'),
+            "Mute": Mengine.getCurrentAccountSettingBool('Mute'),
+        }
+        self.firstCheckChange = {key: True for key in Options.s_start_check_box_values.keys()}
 
         # Check sound volume buttons:
         self.soundCheckController = CheckSoundController(self.object)
@@ -123,32 +132,60 @@ class Options(BaseEntity):
         Demon_Slider_Voice.setParam("Current", value)
 
     def _updateMute(self, value):
-        if self.object.hasObject("Demon_CheckBox_Mute") is False:
-            return
+        if self.firstCheckChange.get("Mute", True) is True:
+            value = Options.s_start_check_box_values["Mute"]
+            self.firstCheckChange["Mute"] = False
 
-        Demon_CheckBox_Mute = self.object.getObject("Demon_CheckBox_Mute")
-        Demon_CheckBox_Mute.setParam("State", value)
+        Mengine.changeCurrentAccountSetting("Mute", unicode(value))
+
+        if GroupManager.hasObject("Options", "Movie2CheckBox_Mute") is True:
+            CheckBox_Mute = GroupManager.getObject("Options", "Movie2CheckBox_Mute")
+            CheckBox_Mute.setParam("Value", value)
+        if self.object.hasObject("Demon_CheckBox_Mute") is True:
+            Demon_CheckBox_Mute = self.object.getObject("Demon_CheckBox_Mute")
+            Demon_CheckBox_Mute.setParam("State", value)
 
     def _updateFullscreen(self, value):
-        if self.object.hasObject("Demon_CheckBox_Fullscreen") is False:
-            return
+        if self.firstCheckChange.get("FullScreen", True) is True:
+            value = Options.s_start_check_box_values["FullScreen"]
+            self.firstCheckChange["FullScreen"] = False
 
-        Demon_CheckBox_Fullscreen = self.object.getObject("Demon_CheckBox_Fullscreen")
-        Demon_CheckBox_Fullscreen.setParam("State", value)
+        Mengine.changeCurrentAccountSetting("Fullscreen", unicode(value))
+
+        if GroupManager.hasObject("Options", "Movie2CheckBox_FullScreen") is True:
+            CheckBox_Fullscreen = GroupManager.getObject("Options", "Movie2CheckBox_FullScreen")
+            CheckBox_Fullscreen.setParam("Value", value)
+        if self.object.hasObject("Demon_CheckBox_Fullscreen") is True:
+            Demon_CheckBox_Fullscreen = self.object.getObject("Demon_CheckBox_Fullscreen")
+            Demon_CheckBox_Fullscreen.setParam("State", value)
 
     def _updateWidescreen(self, value):
-        if self.object.hasObject("Demon_CheckBox_WideScreen") is False:
-            return
+        if self.firstCheckChange.get("WideScreen", True) is True:
+            value = Options.s_start_check_box_values["WideScreen"]
+            self.firstCheckChange["WideScreen"] = False
 
-        Demon_CheckBox_WideScreen = self.object.getObject("Demon_CheckBox_WideScreen")
-        Demon_CheckBox_WideScreen.setParam("State", value)
+        Mengine.changeCurrentAccountSetting("Widescreen", unicode(value))
+
+        if GroupManager.hasObject("Options", "Movie2CheckBox_WideScreen") is True:
+            CheckBox_WideScreen = GroupManager.getObject("Options", "Movie2CheckBox_WideScreen")
+            CheckBox_WideScreen.setParam("Value", value)
+        if self.object.hasObject("Demon_CheckBox_WideScreen") is True:
+            Demon_CheckBox_WideScreen = self.object.getObject("Demon_CheckBox_WideScreen")
+            Demon_CheckBox_WideScreen.setParam("State", value)
 
     def __updateCursor(self, value):
-        if self.object.hasObject("Demon_CheckBox_Arrow") is False:
-            return
+        if self.firstCheckChange.get("Arrow", True) is True:
+            value = Options.s_start_check_box_values["Arrow"]
+            self.firstCheckChange["Arrow"] = False
 
-        Demon_CheckBox_Arrow = self.object.getObject("Demon_CheckBox_Arrow")
-        Demon_CheckBox_Arrow.setParam("State", value)
+        Mengine.changeCurrentAccountSetting("CustomCursor", unicode(value))
+
+        if GroupManager.hasObject("Options", "Movie2CheckBox_Arrow") is True:
+            CheckBox_Arrow = GroupManager.getObject("Options", "Movie2CheckBox_Arrow")
+            CheckBox_Arrow.setParam("Value", value)
+        if self.object.hasObject("Demon_CheckBox_Arrow") is True:
+            Demon_CheckBox_Arrow = self.object.getObject("Demon_CheckBox_Arrow")
+            Demon_CheckBox_Arrow.setParam("State", value)
 
     def setObservers(self):
         def _clickUp(perc, id):
@@ -168,6 +205,11 @@ class Options(BaseEntity):
         self.object.setParam('SoundVolume', Options.startSoundVolumeValue)
         self.object.setParam('VoiceVolume', Options.startVoiceVolumeValue)
 
+        self.object.setParam('Cursor', Options.s_start_check_box_values["Arrow"])
+        self.object.setParam('Fullscreen', Options.s_start_check_box_values["FullScreen"])
+        self.object.setParam('Widescreen', Options.s_start_check_box_values["WideScreen"])
+        self.object.setParam('Mute', Options.s_start_check_box_values["Mute"])
+
         if 'Music' in self.Sliders:
             self.Sliders['Music'][0] = float(Options.startMusicVolumeValue)
         if 'Sound' in self.Sliders:
@@ -179,8 +221,17 @@ class Options(BaseEntity):
         super(Options, self)._onActivate()
 
         self.setObservers()
+
+        # set initial values for Movie2Slider...
         for name, (perc, slider) in self.Sliders.iteritems():
             slider.setParam('Value', perc)
+
+        # ...and for Movie2CheckBox
+        for tag, value in Options.s_start_check_box_values.items():
+            if GroupManager.hasObject("Options", "Movie2CheckBox_{}".format(tag)) is False:
+                continue
+            checkbox = GroupManager.getObject("Options", "Movie2CheckBox_{}".format(tag))
+            checkbox.setParam("Value", value)
 
         def __onSliderUp(slider):
             soundSlider = self.object.getObject("Demon_Slider_Sound")
@@ -191,16 +242,8 @@ class Options(BaseEntity):
 
         self.onSliderUpObserver = Notification.addObserver(Notificator.onSliderUp, __onSliderUp)
 
-        isMobile = Mengine.hasTouchpad()
-
-        # Full screen mode
-        if isMobile is True:
-            if self.object.hasObject("Demon_CheckBox_Fullscreen") is True:
-                Demon_CheckBox_Fullscreen = self.object.getObject("Demon_CheckBox_Fullscreen")
-                Demon_CheckBox_Fullscreen.setParam("Enable", False)
-
         # More options
-        self.activateMoreOptions(isMobile)
+        self.activateMoreOptions()
 
         # Ok
         with TaskManager.createTaskChain(Name='Menu_Options_Ok', GroupName='Options', Repeat=True) as tc:
@@ -231,89 +274,34 @@ class Options(BaseEntity):
             Cancel.addTask('TaskNotify', ID=Notificator.OptionsClose)
             Cancel.addTask('TaskSceneLayerGroupEnable', LayerName='Options', Value=False)
 
-        # Music volume
+        # Music volume - deprecated
         if self.object.hasObject("Demon_Slider_Music") is True:
             with TaskManager.createTaskChain(Name="Menu_Options_MusicVolume", Group=self.object, Repeat=True) as tc:
                 def __onSliderFilter(slider, value):
                     self.object.setParam("MusicVolume", value)
-
                     return True
 
                 tc.addTask("TaskFilter", ObjectName="Demon_Slider_Music", ID=Notificator.onSlider, Filter=__onSliderFilter)
 
-        # Sound volume
+        # Sound volume - deprecated
         if self.object.hasObject("Demon_Slider_Sound") is True:
             with TaskManager.createTaskChain(Name="Menu_Options_SoundVolume", Group=self.object, Repeat=True) as tc:
                 def __onSliderFilter(slider, value):
                     self.object.setParam("SoundVolume", value)
-
                     return True
 
                 tc.addTask("TaskFilter", ObjectName="Demon_Slider_Sound", ID=Notificator.onSlider, Filter=__onSliderFilter)
 
-        # Voice volume
+        # Voice volume - deprecated
         if self.object.hasObject("Demon_Slider_Voice") is True:
             with TaskManager.createTaskChain(Name="Menu_Options_VoiceVolume", Group=self.object, Repeat=True) as tc:
                 def __onSliderFilter(slider, value):
                     self.object.setParam("VoiceVolume", value)
-
                     return True
 
                 tc.addTask("TaskFilter", ObjectName="Demon_Slider_Voice", ID=Notificator.onSlider, Filter=__onSliderFilter)
 
-        # Mute mode
-        if self.object.hasObject("Demon_CheckBox_Mute") is True:
-            with TaskManager.createTaskChain(Name="Menu_Options_Mute", Group=self.object, Repeat=True) as tc:
-                def __onCheckBoxFilter(checkbox, value):
-                    Mengine.changeCurrentAccountSetting("Mute", unicode(value))
-                    return True
-
-                tc.addTask("TaskFilter", ObjectName="Demon_CheckBox_Mute", ID=Notificator.onCheckBox, Filter=__onCheckBoxFilter)
-
-        # Disable screen settings in mobile mode
-        if isMobile is True:
-            DemonNames = ["Demon_CheckBox_Arrow", "Demon_CheckBox_Fullscreen", "Demon_CheckBox_WideScreen"]
-            for DemonName in DemonNames:
-                if self.object.hasObject(DemonName) is True:
-                    Demon = self.object.getObject(DemonName)
-                    Demon.setParam("Enable", False)
-
-            TextNames = ["Text_OptionsSystemCursor", "Text_OptionsFullscreen", "Text_OptionsWideScreen"]
-            for TextName in TextNames:
-                Group = self.object.getGroup()
-                if Group.hasObject(TextName) is True:
-                    Text = Group.getObject(TextName)
-                    Text.setParam("Enable", False)
-
-        else:
-            # System cursor
-            if self.object.hasObject("Demon_CheckBox_Arrow") is True:
-                with TaskManager.createTaskChain(Name="Menu_Options_Arrow", Group=self.object, Repeat=True) as tc:
-                    def __onCheckBoxFilter(checkbox, value):
-                        Mengine.changeCurrentAccountSetting("Cursor", unicode(value))
-
-                        return True
-
-                    tc.addTask("TaskFilter", ObjectName="Demon_CheckBox_Arrow", ID=Notificator.onCheckBox, Filter=__onCheckBoxFilter)
-
-            if self.object.hasObject("Demon_CheckBox_Fullscreen") is True:
-                with TaskManager.createTaskChain(Name="Menu_Options_Fullscreen", Group=self.object, Repeat=True) as tc:
-                    def __onCheckBoxFilter(checkbox, value):
-                        Mengine.changeCurrentAccountSetting("Fullscreen", unicode(value))
-
-                        return True
-
-                    tc.addTask("TaskFilter", ObjectName="Demon_CheckBox_Fullscreen", ID=Notificator.onCheckBox, Filter=__onCheckBoxFilter)
-
-            # Widescreen
-            if self.object.hasObject("Demon_CheckBox_WideScreen") is True:
-                with TaskManager.createTaskChain(Name="Menu_Options_WideScreen", Group=self.object, Repeat=True) as tc:
-                    def __onCheckBoxFilter(checkbox, value):
-                        Mengine.changeCurrentAccountSettingBool("Widescreen", value)
-
-                        return True
-
-                    tc.addTask("TaskFilter", ObjectName="Demon_CheckBox_WideScreen", ID=Notificator.onCheckBox, Filter=__onCheckBoxFilter)
+        self.activateCheckBoxes()
 
         # Set to default
         if self.object.hasObject("Button_Default"):
@@ -343,13 +331,72 @@ class Options(BaseEntity):
                 Trace.log("Entity", 4, "%s has not found %s. Please add to Options.aep, Options.psd if you need." % (
                     self.__class__.__name__, BUTTON_LANGUAGE_SELECT_NAME))
 
-    def activateMoreOptions(self, isMobile):
+    def activateCheckBoxes(self):
+        isMobile = Mengine.hasTouchpad()
+
+        def _createDemonCheckBoxTask(tc_name, demon_name, param_name):   # deprecated
+            with TaskManager.createTaskChain(Name=tc_name, Group=self.object, Repeat=True) as tc:
+                def __onCheckBoxFilter(checkbox, value):
+                    Mengine.changeCurrentAccountSetting(param_name, unicode(value))
+                    return True
+                tc.addTask("TaskFilter", ObjectName=demon_name, ID=Notificator.onCheckBox, Filter=__onCheckBoxFilter)
+        def _createMovie2CheckBoxTask(tc_name, checkbox_name, param_name):
+            with TaskManager.createTaskChain(Name=tc_name, GroupName="Options", Repeat=True) as tc:
+                def __onCheckBoxFilter(checkbox, value):
+                    self.object.setParam(param_name, value)
+                    return True
+                tc.addTask("TaskFilter", ObjectName=checkbox_name, ID=Notificator.onMovie2CheckBox, Filter=__onCheckBoxFilter)
+
+        # Mute mode
+        if self.object.hasObject("Demon_CheckBox_Mute") is True:  # !!!!! deprecated
+            _createDemonCheckBoxTask("Menu_Options_Mute", "Demon_CheckBox_Mute", "Mute")
+        elif GroupManager.hasObject("Options", "Movie2CheckBox_Mute") is True:
+            _createMovie2CheckBoxTask("Menu_Options_Mute", "Movie2CheckBox_Mute", "Mute")
+
+        # Disable screen settings in mobile mode
+        if isMobile is True:
+            DemonNames = ["Demon_CheckBox_Arrow", "Demon_CheckBox_Fullscreen", "Demon_CheckBox_WideScreen"]  # !!!!! deprecated
+            Movie2CheckBoxes = ["Movie2CheckBox_Arrow", "Movie2CheckBox_FullScreen", "Movie2CheckBox_WideScreen"]
+
+            for DemonName in DemonNames + Movie2CheckBoxes:
+                if self.object.hasObject(DemonName) is True:
+                    Demon = self.object.getObject(DemonName)
+                    Demon.setParam("Enable", False)
+
+            TextNames = ["Text_OptionsSystemCursor", "Text_OptionsFullscreen", "Text_OptionsWideScreen", "Text_OptionsMute"]
+            for TextName in TextNames:
+                Group = self.object.getGroup()
+                if Group.hasObject(TextName) is True:
+                    Text = Group.getObject(TextName)
+                    Text.setParam("Enable", False)
+
+            return
+
+        # System cursor
+        if self.object.hasObject("Demon_CheckBox_Arrow") is True:  # !!!!! deprecated
+            _createDemonCheckBoxTask("Menu_Options_Arrow", "Demon_CheckBox_Arrow", "Cursor")
+        elif GroupManager.hasObject("Options", "Movie2CheckBox_Arrow") is True:
+            _createMovie2CheckBoxTask("Menu_Options_Arrow", "Movie2CheckBox_Arrow", "Cursor")
+
+        # Fullscreen
+        if self.object.hasObject("Demon_CheckBox_Fullscreen") is True:  # !!!!! deprecated
+            _createDemonCheckBoxTask("Menu_Options_Fullscreen", "Demon_CheckBox_Fullscreen", "Fullscreen")
+        elif GroupManager.hasObject("Options", "Movie2CheckBox_FullScreen") is True:
+            _createMovie2CheckBoxTask("Menu_Options_Fullscreen", "Movie2CheckBox_FullScreen", "Fullscreen")
+
+        # Widescreen
+        if self.object.hasObject("Demon_CheckBox_WideScreen") is True:  # !!!!! deprecated
+            _createDemonCheckBoxTask("Menu_Options_WideScreen", "Demon_CheckBox_WideScreen", "Widescreen")
+        elif GroupManager.hasObject("Options", "Movie2CheckBox_WideScreen") is True:
+            _createMovie2CheckBoxTask("Menu_Options_WideScreen", "Movie2CheckBox_WideScreen", "Widescreen")
+
+    def activateMoreOptions(self):
         options = GroupManager.getGroup("Options")
 
         if options.hasObject("Movie2Button_More") is False:
             return
 
-        if isMobile is True:
+        if Mengine.hasTouchpad() is True:
             moreOptionsButton = options.getObject("Movie2Button_More")
             moreOptionsButton.setEnable(False)
             return
