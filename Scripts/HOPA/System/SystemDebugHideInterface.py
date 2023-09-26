df71c3c925063cb3be589722595b06af355c8ef7
Ryuzaki <71939354+Ryuzak-i@users.noreply.github.com>
2023-09-26 20:26:36 +0300

    SystemDebugHideInterface: improve and refactor.

--- Scripts/HOPA/System/SystemDebugHideInterface.py
+++ Scripts/HOPA/System/SystemDebugHideInterface.py
@@ -7,87 +7,74 @@ from Foundation.SystemManager import SystemManager
 
 
 class SystemDebugHideInterface(System):
-    INTERFACE_GROUPS = ["Inventory", "Hint", "Toolbar", "HintEffect", "OpenMap", "Navigation"]
+    DEFAULT_INTERFACE_GROUPS = ["Inventory", "Hint", "Toolbar", "HintEffect", "OpenMap", "Navigation"]
 
     def __init__(self):
         super(SystemDebugHideInterface, self).__init__()
-
-        self.Handler = None
-        self.isInterfaceEnable = True
-
-        self.Groups = []
-        pass
+        self.handler = None
+        self.interface_state = True
+        self.groups = []
 
     def _load_groups(self):
-        self.Groups = []
         records = DatabaseManager.getDatabaseRecords("Database", "DebugHideInterfaceGroups")
+
         if records is None:
-            self.Groups = self.INTERFACE_GROUPS
-        else:
-            for record in records:
-                GroupName = record.get("GroupName")
-                self.Groups.append(GroupName)
+            self.groups = self.DEFAULT_INTERFACE_GROUPS
+            return
+
+        for record in records:
+            group_name = record.get("GroupName")
+            self.groups.append(group_name)
 
     def _onRun(self):
         super(SystemDebugHideInterface, self)._onRun()
         self._load_groups()
-
-        self.Handler = Mengine.addKeyHandler(self.__onGlobalHandleKeyEvent)
-
+        self.handler = Mengine.addKeyHandler(self.__onGlobalHandleKeyEvent)
         self.addObserver(Notificator.onSceneInit, self.__onSceneInit)
-
         return True
-        pass
 
     def __onSceneInit(self, SceneName):
-        self.isInterfaceEnable = True
-
+        self.interface_state = True
         return False
-        pass
 
     def __onGlobalHandleKeyEvent(self, event):
         if event.isDown is False:
             return
-            pass
 
         if event.code != DefaultManager.getDefaultKey("DevDebugHideInterface", "VK_CAPITAL"):
             return None
-            pass
 
         if SceneManager.isCurrentGameScene() is False:
             return None
-            pass
 
-        if SystemManager.hasSystem("SystemEditBox"):
+        if SystemManager.hasSystem("SystemEditBox") is True:
             system_edit_box = SystemManager.getSystem("SystemEditBox")
             if system_edit_box.hasActiveEditbox():
                 return None
 
-        if self.isInterfaceEnable is True:
-            self.enableInterface(False)
-            self.isInterfaceEnable = False
-            return None
-            pass
-
-        self.enableInterface(True)
-        self.isInterfaceEnable = True
+        if isinstance(self.getInterfaceState(), bool) is True:
+            self.switchInterfaceStateTo(not self.getInterfaceState())
 
         return None
-        pass
 
-    def enableInterface(self, Value):
-        for GroupName in self.Groups:
-            if GroupManager.hasGroup(GroupName) is False:
+    def switchInterfaceStateTo(self, state):
+        for group_name in self.groups:
+            if GroupManager.hasGroup(group_name) is False:
                 continue
 
-            Group = GroupManager.getGroup(GroupName)
-            if Value is True:
-                Group.onEnable()
-                pass
-            else:
-                Group.onDisable()
-                pass
-            pass
-        pass
+            group = GroupManager.getGroup(group_name)
 
-    pass
+            if group.isActive() is False:
+                continue
+
+            if state is True:
+                if group.getEnable() is False:
+                    group.onEnable()
+            else:
+                if group.getEnable() is True:
+                    group.onDisable()
+
+        self.interface_state = state
+
+    def getInterfaceState(self):
+        return self.interface_state
