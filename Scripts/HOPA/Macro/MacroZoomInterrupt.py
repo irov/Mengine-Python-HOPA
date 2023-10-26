c8d3337e644ec54513b518fecbae8cc84c7ff126
Yeva <55158011+eclipse7723@users.noreply.github.com>
2023-10-26 13:59:54 +0300

    set _checkIsSafe only for dev

--- Scripts/HOPA/Macro/MacroZoomInterrupt.py
+++ Scripts/HOPA/Macro/MacroZoomInterrupt.py
@@ -30,6 +30,10 @@ class MacroZoomInterrupt(MacroCommand):
         """ check if this zoom Scenario could be closed forever
             if we have PickItem active macro commands here - we can't close it
         """
+        if _DEVELOPMENT is True:
+            return True
+        # fixme: after re-enter game - it forget that some of paragraphs were completed
+
         pick_item_macro_commands = [
             "PickItem",
             "GiveItemTip",
