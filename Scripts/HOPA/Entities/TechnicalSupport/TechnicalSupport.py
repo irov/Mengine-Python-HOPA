b91a4106f7e1c7922a0fb2225015df28da53cd46
eclipse7723 <eclipse7723@gmail.com>
2024-01-11 14:49:15 +0200

    hide android info on other platforms

--- Scripts/HOPA/Entities/TechnicalSupport/TechnicalSupport.py
+++ Scripts/HOPA/Entities/TechnicalSupport/TechnicalSupport.py
@@ -59,10 +59,13 @@ class TechnicalSupport(BaseEntity):
         Important Details for our Support Team:
         * game publisher: {PUBLISHER}
         * build version: {BUILD_VERSION} ({BUILD_VERSION_NUMBER})
-        * version code: {BUILD_VERSION_CODE}
-        * {OS_NAME} version: {OS_VERSION}
-        * session id: {SESSION_ID}
         """.format(**kwargs)
+
+        if _ANDROID:
+            body += u"""* version code: {BUILD_VERSION_CODE}
+            * {OS_NAME} version: {OS_VERSION}
+            * session id: {SESSION_ID}
+            """.format(**kwargs)
 
         return body
 
