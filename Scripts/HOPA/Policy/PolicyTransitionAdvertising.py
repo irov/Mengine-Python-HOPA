31cb5ccb25a9f18571896d3fedffe11dbbefbbf7
yurii.levchenko <yurii.levchenko@murka.com>
2025-08-25 22:04:28 +0300

    Fix

--- Scripts/HOPA/Policy/PolicyTransitionAdvertising.py
+++ Scripts/HOPA/Policy/PolicyTransitionAdvertising.py
@@ -1,31 +1,16 @@
 from Foundation.Task.Task import Task
-from Foundation.DemonManager import DemonManager
-from Foundation.DefaultManager import DefaultManager
-from Foundation.TaskManager import TaskManager
-
+from Foundation.SystemManager import SystemManager
 
 class PolicyTransitionAdvertising(Task):
-
     def _onParams(self, params):
         self.TransitionData = params
-        self.AdvertDemonName = DefaultManager.getDefault("AdvertisingDemonName", default="AdvertisingScene")
-
-    def _onInitialize(self):
-        if _DEVELOPMENT is True:
-            if DemonManager.hasDemon(self.AdvertDemonName) is False:
-                self.initializeFailed("not found demon {} - check Config or Demons.xlsx".format(self.AdvertDemonName))
-                return False
-
-        return True
 
     def _onRun(self):
-        transition_params = self.TransitionData
-        demon = DemonManager.getDemon(self.AdvertDemonName)
+        Trace.msg_dev("PolicyTransitionAdvertising", self.TransitionData)
 
-        demon.setTransitionData(transition_params)
-        if demon.runAdvertTransition(self._cbTransition) is False:
-            # default transition, if advert failed
-            TaskManager.runAlias("AliasTransition", self._cbTransition, **transition_params)
+        SystemAdvertising = SystemManager.getSystem("SystemAdvertising")
+
+        SystemAdvertising.tryInterstitial(self.TransitionData, "transition", self._cbTransition)
 
         return False
 
