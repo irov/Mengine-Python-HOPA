ae15de6f68fe164911f29b572b9892a890b92422
Ryuzaki <71939354+Ryuzak-i@users.noreply.github.com>
2025-12-11 10:29:38 +0200

    fix old params

--- Scripts/HOPA/Object/ObjectCollectibles.py
+++ Scripts/HOPA/Object/ObjectCollectibles.py
@@ -9,8 +9,16 @@ class ObjectCollectibles(DemonObject):
         Type.declareParam('CompleteScene')  # True if user collect all collectibles
         Type.declareParam('FinishAnimation')  # True if finish animation has been playing
 
+        Type.declareParam('TransitionBackFromSceneName')
+        Type.declareParam('TransitionBackToSceneName')
+        Type.declareParam('TransitionBackToTextId')
+
     def _onParams(self, params):
         super(ObjectCollectibles, self)._onParams(params)
         self.initParam('PreviousSceneName', params, None)
         self.initParam('CompleteScene', params, False)
         self.initParam('FinishAnimation', params, False)
+
+        self.initParam('TransitionBackFromSceneName', params, None)
+        self.initParam('TransitionBackToSceneName', params, None)
+        self.initParam('TransitionBackToTextId', params, None)
