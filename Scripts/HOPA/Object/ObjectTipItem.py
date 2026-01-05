7d44c26abff516fb4f276ed3404ca97eb77786c2
Ryuzaki <71939354+Ryuzak-i@users.noreply.github.com>
2026-01-05 22:50:09 +0200

    fix missing param

--- Scripts/HOPA/Object/ObjectTipItem.py
+++ Scripts/HOPA/Object/ObjectTipItem.py
@@ -6,12 +6,14 @@ class ObjectTipItem(DemonObject):
     def declareORM(Type):
         DemonObject.declareORM(Type)
 
+        Type.declareParam("TipItem")
         Type.declareParam("TipItemID")
         pass
 
     def _onParams(self, params):
         super(ObjectTipItem, self)._onParams(params)
 
+        self.initParam("TipItem", params, None)
         self.initParam("TipItemID", params, None)
         pass
 
